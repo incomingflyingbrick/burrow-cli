15d5d7766daaa59743759f6b1aa5896ef9ecfa32
vurtne <vurtnesaerdna@gmail.com>
2024-05-23 21:27:53 +0800

    fix download url

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Share GPU with your friends
+* make with python 3.12.3 using poetry and typer
 * supported python version 3.9.0 to 3.12.3
 * eval "$(genv shell --init)"
--- final_install.sh
+++ final_install.sh
@@ -45,7 +45,7 @@ fi
 #install genv
 sudo pip install genv
 git clone https://github.com/run-ai/genv.git $HOME/genv
-wget https://raw.githubusercontent.com/incomingflyingbrick/burrow/main/genv_run_time.py
+wget https://raw.githubusercontent.com/incomingflyingbrick/burrow-cli/main/genv_run_time.py
 python3 genv_run_time.py
 systemctl restart docker
 cp -f $HOME/genv/genv-docker/genv-docker.sh /usr/local/bin/genv-docker
@@ -53,5 +53,5 @@ genv-docker run --rm ubuntu env | grep GENV_
 rm genv_run_time.py
 
 #install burrow-cli
-# pip install burrow-cli
-echo "Burrow install success"
+pip install burrow-cli
+echo "Burrow python library install success"
